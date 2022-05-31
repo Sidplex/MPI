30868c7a72eacc532cb1f2db762e28e7e340d0ec
Sidplex <74717951+Sidplex@users.noreply.github.com>
2022-05-31 16:27:13 +0530

    Update IntegrationMPI.c

--- IntegrationMPI.c
+++ IntegrationMPI.c
@@ -2,36 +2,6 @@
 #include <stdlib.h>
 #include "mpi.h"
 
-void Get_data(
-    float* ap;
-    float* bp;
-    int* np;
-    int my_rank;
-    int size;
-){
-    int source = 0;
-    int dest;
-    int tag;
-    MPI_Status status;
-    
-    if (my_rank == 0){
-        printf("Enter a, b and n \n");
-        scanf("%f %f %d", &a, &b, &n);
-        for ( dest =1 ; dest < size; dest++){
-            MPI_Send(ap, 1 , MPI_FLOAT, dest , tag=0, MPI_COMM_WORLD);
-            MPI_Send(bp, 1 , MPI_FLOAT, dest , tag=1, MPI_COMM_WORLD);
-            MPI_Send(np, 1 , MPI_INT, dest , tag=2, MPI_COMM_WORLD);
-        }
-
-
-    }
-    else{
-        MPI_Recv(ap, 1, MPI_FLOAT, source, tag=0, MPI_COMM_WORLD, &status);
-        MPI_Recv(bp, 1, MPI_FLOAT, source, tag=1, MPI_COMM_WORLD, &status);
-        MPI_Recv(np, 1, MPI_INT, source, tag=2, MPI_COMM_WORLD, &status);
-    }
-}
-
 int main(int argc, char** argv){
     int my_rank;
     int size;
@@ -56,6 +26,22 @@ int main(int argc, char** argv){
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    if (my_rank == 0){
+        printf("Enter a, b and n \n");
+        scanf("%f %f %d", &a, &b, &n);
+        for ( dest = 1 ; dest < size; dest++){
+            MPI_Send(&a, 1 , MPI_FLOAT, dest , tag=0, MPI_COMM_WORLD);
+            MPI_Send(&b, 1 , MPI_FLOAT, dest , tag=1, MPI_COMM_WORLD);
+            MPI_Send(&n, 1 , MPI_INT, dest , tag=2, MPI_COMM_WORLD);
+        }
+
+
+    }
+    else{
+        MPI_Recv(&a, 1, MPI_FLOAT, source, tag=0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&b, 1, MPI_FLOAT, source, tag=1, MPI_COMM_WORLD, &status);
+        MPI_Recv(&n, 1, MPI_INT, source, tag=2, MPI_COMM_WORLD, &status);
+    }
 
     h = (b-a)/n;
     
@@ -106,4 +92,3 @@ float Trap(float local_a, float local_b, int local_n, float h){
 float f(float x){
     return x*x;
 }
-
