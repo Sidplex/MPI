914eda4c87683724143f59e33cb241f694732924
Sidplex <74717951+Sidplex@users.noreply.github.com>
2022-06-09 15:43:47 +0530

    Update IntegrationMPI.py

--- IntegrationMPI.py
+++ IntegrationMPI.py
@@ -1,6 +1,5 @@
 from mpi4py import MPI
 
-
 def fx(x):
     return x*x
 
@@ -17,14 +16,29 @@ def Trap(local_a , local_b, local_n, h):
 comm = MPI.COMM_WORLD
 rank=comm.rank
 size=comm.size
-a = 0.0
-b = 2.0
-n = 1000
 source = 1
+dest = 0
+a = 0.0
+b = 0.0
+n = 0
 
+if rank == 0:
+
+    a = float(input("Enter Upper Limit \n"))
+    b = float(input("Enter Lower Limit \n"))
+    n = int(input("Enter number of trapezoids \n"))
+    for i in range(1,size):
+        dest += 1
+        comm.send(a, dest, tag = 0)
+        comm.send(b, dest, tag = 1)
+        comm.send(n, dest, tag = 2)
+
+else:
+    a=comm.recv(source = 0, tag = 0)
+    b=comm.recv(source = 0, tag = 1)
+    n=comm.recv(source = 0, tag = 2)
 
 h = (b-a)/n
-
 local_n = n/size
 local_a = a + rank * local_n * h
 local_b = (local_a + local_n) * h
@@ -34,9 +48,10 @@ integral = Trap(local_a, local_b, local_n, h)
 if rank == 0:
     total = integral
     while (source < size):
+        integral = comm.recv(source = source,tag = 0)
         source += 1
-        comm.recv(integral,source,tag = 0)
         total = total + integral
+        
 else:
     comm.send(integral, dest = 0, tag = 0)
 
